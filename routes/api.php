d749fb6e5d107a8ac939931565666d7b4b243196
somet <mohammadalamoor96@gmail.com>
2026-08-24 20:58:52 +0300

    feat: initialize laravel api setup and database configuration

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/ping', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'E-Commerce API initialized successfully!'
+    ]);
+});
